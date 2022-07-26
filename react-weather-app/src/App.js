a6796ccf9b0fc982b451a359a4db2c055a1c5739
Jose Herminio Parreno Piqueras <josete4ever@gmail.com>
2022-07-26 08:42:10 +0200

    TODO: join promise all

--- react-weather-app/src/App.js
+++ react-weather-app/src/App.js
@@ -1,26 +1,36 @@
 import "./App.css";
 import CurrentWeather from "./components/current-weather/currentWeather";
 import Search from "./components/search/search";
-import {OPEN_WEATHER_API_KEY, OPEN_WEARTHER_API_URL} from "./api/api";
+import {OPEN_WEATHER_API_KEY, OPEN_WEARTHER_API_URL, OPEN_WEATHER_FORECAST_API_URL} from "./api/api";
+import React, { useState } from "react";
 
 function App() {
-
-  const selectedCity = {}; // a ver esto si también hace falta ponerlo en estado
+  const [selectedLocation, setSelectedLocation] = useState({})
+  const [selectedForecast, setSelectedForecast] = useState({})
 
   const handleOnSearchChange = (selectedCity) => {
-    console.log("selectedCity" , selectedCity);
     const [latitude, longitude] = selectedCity.value.split(" ");
 
     let currentWeatherFetch = fetch(`${OPEN_WEARTHER_API_URL}?lat=${latitude}&lon=${longitude}&appid=${OPEN_WEATHER_API_KEY}`).then((response => {
-      console.log("response", response);
-      debugger
-    }))
+      return response.json().then(jsonReponse => {
+        console.log("currentWeather", jsonReponse);
+        setSelectedLocation(jsonReponse);
+      })
+    }));
+
+    let forecastFetch = fetch(`${OPEN_WEATHER_FORECAST_API_URL}&lat=${latitude}&lon=${longitude}&appid=${OPEN_WEATHER_API_KEY}`).then((response => {
+      return response.json().then(jsonReponse => {
+        console.log("currentForecast", jsonReponse);
+        setSelectedForecast(jsonReponse);
+      })
+    }));
+
 
   };
   return (
     <div className="container">
       <Search onSearchChange={handleOnSearchChange}></Search>
-      <CurrentWeather selectedCity={selectedCity}></CurrentWeather>
+      <CurrentWeather selectedLocation={selectedLocation}></CurrentWeather>
     </div>
   );
 }
--- react-weather-app/src/components/current-weather/currentWeather.js
+++ react-weather-app/src/components/current-weather/currentWeather.js
@@ -2,43 +2,48 @@ import "./currentWeather.css";
 
 import React from "react";
 
-function CurrentWeather() {
-  return (
-    <div className="weather">
-      <div className="top">
-        <div>
-          <p className="city">Belgrade</p>
-          <p className="weatherDescription">Sunny</p>
-        </div>
+function CurrentWeather({ selectedLocation }) {
+  if (selectedLocation.id) {
 
-        <img alt="weather" className="weatherIcon" src="icons/01d.png"></img>
-      </div>
-      <div className="bottom">
-        <p className="temperature">18 C</p>
-        <div className="details">
-          <div className="parameterRow">
-            <span className="parameterLabel">Details</span>
+    
+    return (
+      <div className="weather">
+        <div className="top">
+          <div>
+            <p className="city">{selectedLocation.name}</p>
+            <p className="weatherDescription">Sunny</p>
           </div>
-          <div className="parameterRow">
-            <span className="parameterLabel">Feels like</span>
-            <span className="parameterVabel">22</span>
-          </div>
-          <div className="parameterRow">
-            <span className="parameterLabel">Wind</span>
-            <span className="parameterVabel">2 m/s</span>
-          </div>
-          <div className="parameterRow">
-            <span className="parameterLabel">Humidity</span>
-            <span className="parameterVabel">15 %</span>
-          </div>
-          <div className="parameterRow">
-            <span className="parameterLabel">Pressure</span>
-            <span className="parameterVabel">22 hPa</span>
+
+          <img alt="weather" className="weatherIcon" src="icons/01d.png"></img>
+        </div>
+        <div className="bottom">
+          <p className="temperature">18 C</p>
+          <div className="details">
+            <div className="parameterRow">
+              <span className="parameterLabel">Details</span>
+            </div>
+            <div className="parameterRow">
+              <span className="parameterLabel">Feels like</span>
+              <span className="parameterVabel">22</span>
+            </div>
+            <div className="parameterRow">
+              <span className="parameterLabel">Wind</span>
+              <span className="parameterVabel">2 m/s</span>
+            </div>
+            <div className="parameterRow">
+              <span className="parameterLabel">Humidity</span>
+              <span className="parameterVabel">15 %</span>
+            </div>
+            <div className="parameterRow">
+              <span className="parameterLabel">Pressure</span>
+              <span className="parameterVabel">22 hPa</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+  return <></>;
 }
 
 export default CurrentWeather;

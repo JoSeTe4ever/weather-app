388588c47e1617466c79be0ea03b578feaa27f3d
Jose Herminio Parreno Piqueras <josete4ever@gmail.com>
2022-07-27 15:50:35 +0200

    finished first widget: TODO Forecast

--- react-weather-app/src/App.js
+++ react-weather-app/src/App.js
@@ -9,42 +9,32 @@ import {
 import React, { useState } from "react";
 
 function App() {
-  const [selectedLocation, setSelectedLocation] = useState({});
+  const [selectedLocationWeather, setSelectedLocationWeather] = useState({});
   const [selectedForecast, setSelectedForecast] = useState({});
 
   const handleOnSearchChange = (selectedCity) => {
     const [latitude, longitude] = selectedCity.value.split(" ");
 
-    /*let currentWeatherFetch = fetch(`${OPEN_WEARTHER_API_URL}?lat=${latitude}&lon=${longitude}&appid=${OPEN_WEATHER_API_KEY}`).then((response => {
-      return response.json().then(jsonReponse => {
-        console.log("currentWeather", jsonReponse);
-        setSelectedLocation(jsonReponse);
-      })
-    }));
-
-    let forecastFetch = fetch(`${OPEN_WEATHER_FORECAST_API_URL}&lat=${latitude}&lon=${longitude}&appid=${OPEN_WEATHER_API_KEY}`).then((response => {
-      return response.json().then(jsonReponse => {
-        console.log("currentForecast", jsonReponse);
-        setSelectedForecast(jsonReponse);
-      })
-    }));*/
 
     let currentWeatherFetch = fetch(
-      `${OPEN_WEARTHER_API_URL}?lat=${latitude}&lon=${longitude}&appid=${OPEN_WEATHER_API_KEY}`
+      `${OPEN_WEARTHER_API_URL}?lat=${latitude}&lon=${longitude}&appid=${OPEN_WEATHER_API_KEY}&units=metric`
     );
     let forecastFetch = fetch(
-      `${OPEN_WEATHER_FORECAST_API_URL}&lat=${latitude}&lon=${longitude}&appid=${OPEN_WEATHER_API_KEY}`
+      `${OPEN_WEATHER_FORECAST_API_URL}&lat=${latitude}&lon=${longitude}&appid=${OPEN_WEATHER_API_KEY}&units=metric`
     );
 
-    Promise.all([currentWeatherFetch, forecastFetch]).then((results) => {
-      if (Array.isArray(results)) {
-      }
+    Promise.all([currentWeatherFetch, forecastFetch]).then(async (results) => {
+      const weatherResponse = await results[0].json();
+      const forecastResponse = await results[1].json();
+
+      setSelectedLocationWeather(weatherResponse);
+      setSelectedForecast(forecastResponse);
     });
   };
   return (
     <div className="container">
       <Search onSearchChange={handleOnSearchChange}></Search>
-      <CurrentWeather selectedLocation={selectedLocation}></CurrentWeather>
+      <CurrentWeather selectedLocation={selectedLocationWeather}></CurrentWeather>
     </div>
   );
 }
--- react-weather-app/src/components/current-weather/currentWeather.js
+++ react-weather-app/src/components/current-weather/currentWeather.js
@@ -3,40 +3,39 @@ import "./currentWeather.css";
 import React from "react";
 
 function CurrentWeather({ selectedLocation }) {
-  if (selectedLocation.id) {
 
-    
+  if (selectedLocation.id) {
     return (
       <div className="weather">
         <div className="top">
           <div>
             <p className="city">{selectedLocation.name}</p>
-            <p className="weatherDescription">Sunny</p>
+            <p className="weatherDescription">{selectedLocation.weather[0].description}</p>
           </div>
 
-          <img alt="weather" className="weatherIcon" src="icons/01d.png"></img>
+          <img alt="weather" className="weatherIcon" src={`icons/${selectedLocation.weather[0].icon}.png`}></img>
         </div>
         <div className="bottom">
-          <p className="temperature">18 C</p>
+          <p className="temperature">{Math.round(selectedLocation.main.temp)} °C</p>
           <div className="details">
             <div className="parameterRow">
               <span className="parameterLabel">Details</span>
             </div>
             <div className="parameterRow">
               <span className="parameterLabel">Feels like</span>
-              <span className="parameterVabel">22</span>
+              <span className="parameterVabel">{Math.round(selectedLocation.main.temp)} °C</span>
             </div>
             <div className="parameterRow">
               <span className="parameterLabel">Wind</span>
-              <span className="parameterVabel">2 m/s</span>
+              <span className="parameterVabel">{selectedLocation.wind.speed} m/s</span>
             </div>
             <div className="parameterRow">
               <span className="parameterLabel">Humidity</span>
-              <span className="parameterVabel">15 %</span>
+              <span className="parameterVabel">{selectedLocation.main.humidity} %</span>
             </div>
             <div className="parameterRow">
               <span className="parameterLabel">Pressure</span>
-              <span className="parameterVabel">22 hPa</span>
+              <span className="parameterVabel">{selectedLocation.main.preassure} hPa</span>
             </div>
           </div>
         </div>
